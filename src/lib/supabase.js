212a7075171a263fcc6043d34887b2cefeca6e2a
eglagares <elena.gonzalez@fiveshareit.es>
2026-07-17 15:53:18 +0200

    debug env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -12,6 +12,14 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   console.warn('VITE_SUPABASE_ANON_KEY=...');
 }
 
+
+console.log("======== VARIABLES ========");
+console.log(import.meta.env);
+console.log("SUPABASE URL =", import.meta.env.VITE_SUPABASE_URL);
+console.log("SUPABASE KEY =", import.meta.env.VITE_SUPABASE_ANON_KEY);
+console.log("===========================");
+
+
 // Crear cliente de Supabase
 export const supabase = createClient(
   SUPABASE_URL || '',
@@ -28,6 +36,3 @@ export const supabase = createClient(
 export default supabase;
 
 
-console.log("SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY ? "OK" : "NO");
-console.log("ENV:", import.meta.env);
